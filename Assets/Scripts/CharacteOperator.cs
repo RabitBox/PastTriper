18b4c5a7cfc14fee638c37d1f69c97bde741d80f
Gai Takakura <gixa.aturakka@gmail.com>
2017-01-26 17:51:52 +0900

    first push

---
+++ Assets/Scripts/CharacteOperator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacteOperator : MonoBehaviour {
+
+    private GameObject camera = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 入力を管理する
+/// </summary>
+public class InputManager : MonoBehaviour {
+    //--------------------------------------------------
+    // Variable
+    //--------------------------------------------------
+    private static InputManager instance = null;
+
+    //--------------------------------------------------
+    // Property
+    //--------------------------------------------------
+    public InputManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = this;
+            return instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
